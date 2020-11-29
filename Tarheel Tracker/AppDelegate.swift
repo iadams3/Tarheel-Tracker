f8856de8621953f8b56f6515e7c5c65519c1d096
Isaac Adams <isaac.adams332@gmail.com>
2020-11-29 18:25:00 -0500

    Initial Commit

---
+++ Tarheel Tracker/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Tarheel Tracker
+//
+//  Created by Isaac Adams on 11/29/20.
+//  Copyright © 2020 Isaac Adams. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Tarheel Tracker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Tarheel Tracker
+//
+//  Created by Isaac Adams on 11/29/20.
+//  Copyright © 2020 Isaac Adams. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
